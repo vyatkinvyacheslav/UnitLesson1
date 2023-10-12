9df7e68d7ecb5fed3627573497d500e3562f521b
Vyacheslav Vyatkin <Vyatkin.vyacheslav.a@yandex.ru>
2023-10-12 22:27:48 +0500

    second commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -60,7 +60,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(expected, actual);
 
@@ -107,6 +107,7 @@ public class CashbackHackServiceTest {
         int expected = 1000;
 
         assertEquals(expected, actual);
+        
 
     }
 }
